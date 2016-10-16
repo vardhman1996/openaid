5e806965a85a68f2a689653cdb344e27110dd8bb
divye <dpjain@uw.edu>
2016-10-16 09:50:57 +0000

    added user to db

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
-
 
 var client_id = '3c3a5087f013115ba635';
 var client_secret = '12bd42f1c77097c542d872c4c4f7be860af399b2';
@@ -9,25 +7,41 @@ var client_secret = '12bd42f1c77097c542d872c4c4f7be860af399b2';
 var request = require('request');
 
 const https = require('https');
+var User = require('../models/user');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var access_token = "02e2fa6d145c3c36a3ea56932cf8398592885be9";
+    var access_token = req.query.access_token;
   request({
     url : 'http://api.github.com/user?access_token=' + access_token, 
     headers: {"User-Agent" : "OpenAid"}
   },function (error, response, body) {
         console.log("HERE");
         if (!error) {
-            res.send(body);
+            body = JSON.parse(body);
+            var user = new User();
+            // console.log(body.login + " " + body.username + " " + body.id);
+            // User.findOne({
+            //   id: body.id
+            // }, function(resm) {
+            //   console.log("Query Result: " + resm);
+            // });
+            user.username = body.login;
+            user.name = body.name;
+            user.id = body.id;
+            user.save(function (err) {
+              if(err) {
+                console.log(err);
+              }
+              else {
+                res.send({done: true});
+              }
+            });
         } else {
             res.send("ERROR");
         }
     });
 });
-
-
-//   
-// });
 
 module.exports = router;
