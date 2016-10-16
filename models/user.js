20c90f7c57c24caf73f12d6fd90ffa426c23c1bc
Vardhman Mehta <vardhman.mehta96@gmail.com>
2016-10-16 07:54:24 +0000

    temporary user schema

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new Schema({
   name: {type: String, required: true},
+  username: {type: String, required: true}
 });
 
 module.exports = mongoose.model('User', UserSchema);
