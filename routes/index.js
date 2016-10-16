cdb94e8503d04368d2fc20de85cd3ebc0524972d
Ishan Saksena <ishansaksena@gmail.com>
2016-10-16 04:36:34 -0700

    front end files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,66 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+var repositories = [{
+        id: 70939672,
+        name: "chessbots",
+        login: "vardhman1996",
+        html_url: "https://github.com/vardhman1996/chessbots",
+        description: " Wrote several chess bots using AI algorithms"
+    },
+    {
+        id: 38805895,
+        name: "code_autocorrector",
+        login: "vardhman1996",
+        html_url: "https://github.com/vardhman1996/code_autocorrector",
+        description: "Code validation for WizRocket"
+    },
+    {
+        id: 32976956,
+        name: "d3",
+        login: "vardhman1996",
+        html_url: "https://github.com/vardhman1996/d3",
+        description: "A JavaScript visualization library for HTML and SVG."
+    },
+    {
+        id: 46309024,
+        name: "getting-a-gig",
+        login: "vardhman1996",
+        html_url: "https://github.com/vardhman1996/getting-a-gig",
+        description: "A guide for getting a gig in college as a tech major."
+    },
+    {
+        id: 36142873,
+        name: "habitat",
+        login: "vardhman1996",
+        html_url: "https://github.com/vardhman1996/habitat",
+        description: null
+    }];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OpenAid' });
+});
+
+router.get('/master', function(req, res, next) {
+    res.render('master', { title: 'Publish Projects' });
+});
+
+router.get('/developer', function(req, res, next) {
+    res.render('developer', { title: 'Contribute' });
+});
+
+router.get('/repos', function(req, res, next) {
+    var issues = ["This doesn't work", "It really doesn't work", "Are you clowning me!?!??!"];
+    res.render('repos', {title: 'Login',  repos: repositories});
+});
+
+// List of functions posted
+router.post('/repos', function(req, res, next) {
+    console.log(req.body);
+    res.render('repos', {title: 'Submitted'});
+
 });
 
 module.exports = router;
