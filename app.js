d2eafef2e5fd84ddc9910c4334a7a8f6704dba54
Anirban Biswas <anirban.biswas07@gmail.com>
2016-10-16 08:29:53 +0000

    users endpoint has access token

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var auth = require('./routes/auth');
-var auth_code = require('./routes/code');
 
 
 var app = express();
@@ -28,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/auth', auth);
-app.use('/code', auth_code);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
