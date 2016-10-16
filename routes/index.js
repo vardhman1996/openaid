6a9e2f4c3bf69e07cfbe76fa9f1c56cfebb2e06b
Ishan Saksena <ishansaksena@gmail.com>
2016-10-16 09:27:16 -0700

    ui mostly done

--- routes/index.js
+++ routes/index.js
@@ -41,6 +41,7 @@ var repositories = [{
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'OpenAid' });
+
 });
 
 router.get('/master', function(req, res, next) {
@@ -52,15 +53,87 @@ router.get('/developer', function(req, res, next) {
 });
 
 router.get('/repos', function(req, res, next) {
-    var issues = ["This doesn't work", "It really doesn't work", "Are you clowning me!?!??!"];
     res.render('repos', {title: 'Login',  repos: repositories});
+});
+
+router.get('/collectedissues', function(req, res, next) {
+    var issues = [
+        {
+            _id: "5803845d0ae3494b27c18f53",
+            body: "*Kicks off the mountain*",
+            reponame: "Demo_repo",
+            username: "Divye02",
+            html_url: "https://github.com/Divye02/Demo_repo/issues/1",
+            title: "This is Sparta",
+            __v: 0,
+            labels: [ ]
+        }
+    ];
+
+    res.render('collectedissues', {title: 'Issues',  issues: issues});
+});
+
+router.get('/search', function(req, res, next) {
+    var tags ={ labels: [
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-Damn",
+            name: "Damn",
+            color: "1d76db"
+        },
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-tree",
+            name: "tree",
+            color: "c2e0c6"
+        },
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-tree",
+            name: "tree",
+            color: "c2e0c6"
+        },
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-tree",
+            name: "tree",
+            color: "c2e0c6"
+        },
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-tree",
+            name: "tree",
+            color: "c2e0c6"
+        },
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-tree",
+            name: "tree",
+            color: "c2e0c6"
+        },
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-tree",
+            name: "tree",
+            color: "c2e0c6"
+        },
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-tree",
+            name: "tree",
+            color: "c2e0c6"
+        },
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-tree",
+            name: "tree",
+            color: "c2e0c6"
+        },
+        {
+            url: "https://api.github.com/repos/Divye02/Demo_repo/labels/OA-tree",
+            name: "treeasdf",
+            color: "c2e0c6"
+        }
+    ]};
+
+    res.render('search', {title: 'Issues',  tags: tags});
 });
 
 // List of functions posted
 router.post('/repos', function(req, res, next) {
     console.log(req.body);
     res.render('repos', {title: 'Submitted'});
-
 });
 
 module.exports = router;
